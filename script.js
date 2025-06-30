7b431af584499227da9621914b5ca72448004eb1
Lipto4569 <gdeepto@gmail.com>
2025-06-30 15:27:42 +0530

    all should be downloaded in order to work
    it was made by me

---
+++ script.js
@@ -0,0 +1,16 @@
+let hel=10;
+let c=90;
+console.log(hel+c);
+if((hel+c)==100)
+{
+    console.log("100 is this")
+}
+else
+{
+    console.log("100 is this")
+}
+let i=20;
+while(i<=100){
+    console.log(i)
+    i+=2
+}
